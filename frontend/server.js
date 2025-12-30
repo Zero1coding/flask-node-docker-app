b8f51a186a87750f5da7bf3d278eae54c7f914e1
ZeroCoding <164722131+Zero1coding@users.noreply.github.com>
2025-12-30 22:22:37 +0530

    Add files via upload

---
+++ frontend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/submit", async (req, res) => {
+  try {
+    const response = await axios.post("http://backend:5000/submit", {
+      item_id: req.body.item_id
+    });
+    res.send(response.data);
+  } catch (error) {
+    res.send("Error connecting to backend");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Frontend running on port 3000");
+});
